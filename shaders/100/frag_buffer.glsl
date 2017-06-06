51b90fa4d90ad23d689429d13c07fd20df1d4228
Chris Eade <chris.eade@gmail.com>
2017-06-06 20:42:46 +0000

    Broken GLSL 100 shaders
    There is something wrong with the mv_matrix in the GLSL 100 shader on the
    raspberry pi. If I change the shader to use the mvp matrix then I get a
    distorted (expected) lighting on the cube, but the mv matrix stops rendering
    all-together. At a loss. This matrix works on the desktop version.
    
    Perhaps there is a limit on the nr of matricies available as uniforms.

--- shaders/100/frag_buffer.glsl
+++ shaders/100/frag_buffer.glsl
@@ -1,20 +1,17 @@
 #version 100
 
-uniform mat4 m_mv;
+precision mediump int;
+precision mediump float;
 
 varying vec4 v_color;
 varying vec3 v_normal;
 varying vec3 v_vertex;
 
 void main(void) {
-    vec3 normal_ms = normalize(vec3(m_mv * vec4(v_normal, 0)));
-    vec3 light_ms = vec3(0.0, 0.0, 0.0);
-    vec3 vert_ms = vec3(m_mv * vec4(v_vertex, 1.0));
-    vec3 stl = light_ms - vert_ms;
-
-    float brightness = dot(normal_ms, stl) / (length(stl) * length(normal_ms));
-    brightness = clamp(brightness, 0.0, 1.0);
-
-    vec3 d_color = vec3(v_color) * brightness;
-    gl_FragColor = vec4(d_color, 1.0);
+    vec3 u_light = vec3(0.0, 0.0, 0.0);
+    float distance = length(u_light - v_vertex);
+    vec3 light_vec = normalize(u_light - v_vertex);
+    float diffuse = max(dot(v_normal, light_vec), 0.1);
+    diffuse = diffuse * (1.0 / (1.0 + (0.25 * distance * distance)));
+    gl_FragColor = v_color * diffuse;
 }
--- shaders/100/vert_buffer.glsl
+++ shaders/100/vert_buffer.glsl
@@ -7,6 +7,7 @@ attribute vec3 b_vertex;
 attribute vec3 b_normal;
 
 uniform mat4 m_mvp;
+uniform mat4 m_mov;
 uniform vec4 u_color;
 
 varying vec4 v_color;
@@ -15,7 +16,7 @@ varying vec3 v_vertex;
 
 void main(void) {
     v_color = u_color;
-    v_normal = b_normal;
-    v_vertex = b_vertex;
+    v_normal = vec3(m_mov * vec4(b_normal, 0.0)); // m_mov is bad
+    v_vertex = vec3(m_mov * vec4(b_vertex, 0.0)); // m_mov is bad
     gl_Position = m_mvp * vec4(b_vertex, 1.0);
 }
