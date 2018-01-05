534fa0887d155a778f929abd262010732dfb4ea8
Chris Eade <chris.eade@gmail.com>
2018-01-05 18:58:24 +0000

    Misc problem with fprintf on Raspberry Pi

--- src/safe_malloc.c
+++ src/safe_malloc.c
@@ -4,7 +4,7 @@
 void* safe_malloc(size_t n, char* file, unsigned long line) {
     void* p = malloc(n);
     if (!p) {
-        fprintf(stderr, "[%s:%zu]Out of memory(%zu bytes)\n", file, line, n);
+        fprintf(stderr, "[%s:%lu]Out of memory(%zu bytes)\n", file, line, n);
         exit(EXIT_FAILURE);
     }
     return p;
