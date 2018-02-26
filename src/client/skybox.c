9278df20ac7f24ca14b340a806a0a431adbcd0ee
ceade <chris.eade@gmail.com>
2018-02-26 19:24:12 +0100

    Converted skybox test to new render VM
    Mainly to see if that worked since gree_cube is still broken.

--- src/client/skybox.c
+++ src/client/skybox.c
@@ -6,19 +6,10 @@
 #include "esm.h"
 #include <time.h>
 
-#define NANO_SECOND_MULTIPLIER 1000000
-const long INTERVAL_MS = 100 * NANO_SECOND_MULTIPLIER;
-
 int main(void) {
     uint32_t scene_id;
     uint32_t skybox_id;
-    uint32_t memory_id;
-    uint32_t matrix_size;
-    uint32_t render_size;
     vrms_client_t* client;
-    uint8_t* shared_mem;
-    float* model_matrix;
-    uint32_t* render_buffer;
 
     client = vrms_connect();
     if (NULL == client) {
@@ -40,25 +31,7 @@ int main(void) {
         exit(1);
     }
 
-    matrix_size = sizeof(float) * 16;
-    render_size = sizeof(uint32_t) * 3;
-    memory_id = vrms_client_create_memory(client, &shared_mem, matrix_size + render_size);
-
-    if (NULL == shared_mem) {
-        fprintf(stderr, "Unable to initialize shared memory\n");
-        vrms_destroy_scene(client);
-        exit(1);
-    }
-
-    model_matrix = (float*)shared_mem;
-    render_buffer = (uint32_t*)&shared_mem[matrix_size];
-
-    render_buffer[0] = memory_id;
-    render_buffer[1] = 0;
-    render_buffer[2] = skybox_id;
-
-    esmLoadIdentity(model_matrix);
-    vrms_client_render_buffer_set(client, memory_id, matrix_size, render_size);
+    vrms_geometry_render_buffer_basic(client, skybox_id, 0.0f, 0.0f, 0.0f);
 
     sleep(60);
 
