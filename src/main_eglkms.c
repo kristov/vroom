ca5066351f66b5941e6e29824603e1a0895f7a21
Chris Eade <chris.eade@gmail.com>
2018-04-01 09:22:39 +0200

    Got to the point of rendering an orange screen
    Using GBM/KMS/DRM/EGL (still no clue how they fit together)

--- src/main_eglkms.c
+++ src/main_eglkms.c
@@ -2,224 +2,235 @@
 #include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <errno.h>
 #include <gbm.h>
 #include <libdrm/drm.h>
+#include <xf86drm.h>
 #include <xf86drmMode.h>
 #include <EGL/egl.h>
 #include <GLES2/gl2.h>
 #include "vrms_server_socket.h"
 
-#define NANO_SECOND_MULTIPLIER 1000000
-const long INNER_LOOP_INTERVAL_MS = 50 * NANO_SECOND_MULTIPLIER;
-
-int32_t main(int argc, char **argv) {
+typedef struct eglkms_context {
     int fd;
     struct gbm_device* gbm;
-    EGLDisplay display;
-    EGLint num_configs;
-    EGLConfig config;
-    EGLContext context;
-    EGLint major_v;
-    EGLint minor_v;
-    EGLBoolean result;
-    const char* version;
-    const char* extensions;
+    struct gbm_surface* gbm_surface;
+    GLuint color_rb[2];
+    uint16_t width;
+    uint16_t height;
+    drmModeConnector* kms_connector;
+    drmModeEncoder* kms_encoder;
+    drmModeModeInfo kms_mode;
+    drmModeCrtc* kms_crtc;
+    uint32_t kms_fb_id[2];
+    GLuint fb;
+    struct gbm_bo* buff_obj[2];
+    EGLDisplay egl_display;
+    EGLContext egl_context;
+    EGLSurface egl_surface;
+//    EGLImageKHR khr_image[2];
+} eglkms_context_t;
 
-    EGLint attr_context[] = {
-        EGL_CONTEXT_CLIENT_VERSION, 2,
-        EGL_NONE, EGL_NONE
-    };
+//static void page_flip_handler(int fd, unsigned int frame, unsigned int sec, unsigned int usec, void *data) {
+//    ;
+//}
 
-    EGLint attr_list[] = {
+void render_stuff() {
+    glClearColor(1.0f, 0.6f, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void render_loop(eglkms_context_t* context) {
+    struct gbm_bo *previous_bo;
+    uint32_t previous_fb;
+    uint8_t quit;
+    struct gbm_bo* bo;
+    uint32_t handle;
+    uint32_t pitch;
+    uint32_t fb;
+
+    quit = 0;
+    do {
+        eglSwapBuffers(context->egl_display, context->egl_surface);
+        bo = gbm_surface_lock_front_buffer(context->gbm_surface);
+        handle = gbm_bo_get_handle(bo).u32;
+        pitch = gbm_bo_get_stride(bo);
+
+        drmModeAddFB(context->fd, context->width, context->height, 24, 32, pitch, handle, &fb);
+        drmModeSetCrtc(context->fd, context->kms_crtc->crtc_id, fb, 0, 0, &context->kms_connector->connector_id, 1, &context->kms_mode);
+
+        if (previous_bo) {
+            drmModeRmFB(context->fd, previous_fb);
+            gbm_surface_release_buffer(context->gbm_surface, previous_bo);
+        }
+        previous_bo = bo;
+        previous_fb = fb;
+        render_stuff();
+    } while (!quit);
+}
+
+void drm_crtc(eglkms_context_t* context) {
+    drmModeCrtcPtr saved_crtc;
+    int ret;
+
+    saved_crtc = drmModeGetCrtc(context->fd, context->kms_encoder->crtc_id);
+    if (saved_crtc == NULL) {
+        fprintf(stderr, "failed to save crtc\n");
+        return;
+    }
+
+    render_loop(context);
+
+    ret = drmModeSetCrtc(context->fd, saved_crtc->crtc_id, saved_crtc->buffer_id, saved_crtc->x, saved_crtc->y, &context->kms_connector->connector_id, 1, &saved_crtc->mode);
+    if (ret) {
+        fprintf(stderr, "failed to restore crtc\n");
+        return;
+    }
+    drmModeFreeCrtc(saved_crtc);
+}
+
+void egl_context(eglkms_context_t* context) {
+    EGLint attributes[] = {
         EGL_RED_SIZE, 8,
         EGL_GREEN_SIZE, 8,
         EGL_BLUE_SIZE, 8,
-        EGL_ALPHA_SIZE, EGL_DONT_CARE,
-        EGL_DEPTH_SIZE, 16,
         EGL_NONE
     };
+    EGLConfig config;
+    EGLint num_config;
 
-    fd = open("/dev/dri/card0", O_RDWR);
+    eglBindAPI(EGL_OPENGL_API);
 
-    gbm = gbm_create_device(fd);
-
-    display = eglGetDisplay(gbm);
-
-    result = eglInitialize(display, &major_v, &minor_v);
-    if (result == EGL_FALSE) {
-        fprintf(stderr, "eglInitialize failed\n");
-        return 1;
+    eglChooseConfig(context->egl_display, attributes, &config, 1, &num_config);
+    context->egl_context = eglCreateContext(context->egl_display, config, EGL_NO_CONTEXT, NULL);
+    if (context->egl_context == NULL) {
+        fprintf(stderr, "failed to create context\n");
+        return;
     }
 
-    version = eglQueryString(display, EGL_VERSION);
-    extensions = eglQueryString(display, EGL_EXTENSIONS);
+    context->gbm_surface = gbm_surface_create (context->gbm, context->width, context->height, GBM_BO_FORMAT_XRGB8888, GBM_BO_USE_SCANOUT|GBM_BO_USE_RENDERING);
+    context->egl_surface = eglCreateWindowSurface(context->egl_display, config, context->gbm_surface, NULL);
+    
+    if (!eglMakeCurrent(context->egl_display, context->egl_surface, context->egl_surface, context->egl_context)) {
+        fprintf(stderr, "failed to make context current\n");
+        return;
+    }
+
+    drm_crtc(context);
+
+    eglMakeCurrent(context->egl_display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
+    eglDestroyContext(context->egl_display, context->egl_context);
+}
+
+void kms_drm(eglkms_context_t* context) {
+    drmModeRes *resources;
+    drmModeConnector *connector;
+    drmModeEncoder *encoder;
+    uint8_t i;
+
+    resources = drmModeGetResources(context->fd);
+    if (!resources) {
+        fprintf(stderr, "drmModeGetResources failed\n");
+        return;
+    }
+    for (i = 0; i < resources->count_connectors; i++) {
+        connector = drmModeGetConnector(context->fd, resources->connectors[i]);
+        if (connector == NULL)
+            continue;
+        if (connector->connection == DRM_MODE_CONNECTED && connector->count_modes > 0)
+            break;
+        drmModeFreeConnector(connector);
+    }
+    if (i == resources->count_connectors) {
+        fprintf(stderr, "No currently active connector found.\n");
+        return;
+    }
+    for (i = 0; i < resources->count_encoders; i++) {
+        encoder = drmModeGetEncoder(context->fd, resources->encoders[i]);
+        if (encoder == NULL)
+            continue;
+        if (encoder->encoder_id == connector->encoder_id)
+            break;
+        drmModeFreeEncoder(encoder);
+    }
+
+    if (encoder->crtc_id) {
+        context->kms_crtc = drmModeGetCrtc(context->fd, encoder->crtc_id);
+    }
+
+    context->kms_connector = connector;
+    context->kms_encoder = encoder;
+    context->kms_mode = connector->modes[0];
+    
+    context->width = context->kms_mode.hdisplay;
+    context->height = context->kms_mode.vdisplay;
+
+    egl_context(context);
+
+    drmModeFreeConnector(connector);
+    drmModeFreeEncoder(encoder);
+}
+
+void egl_root(eglkms_context_t* context) {
+    int ret;
+    const char *version;
+    const char *extensions;
+    EGLint major_v;
+    EGLint minor_v;
+
+    context->egl_display = eglGetDisplay(context->gbm);
+
+    ret = eglInitialize(context->egl_display, &major_v, &minor_v);
+    if (ret == EGL_FALSE) {
+        fprintf(stderr, "eglInitialize failed\n");
+        return;
+    }
+
+    version = eglQueryString(context->egl_display, EGL_VERSION);
+    extensions = eglQueryString(context->egl_display, EGL_EXTENSIONS);
 
     fprintf(stderr, "extensions: %s\n", extensions);
     fprintf(stderr, "version: %s\n", version);
 
-    //if (!strstr(extensions, "EGL_KHR_surfaceless_opengl")) {
-    //    fprintf(stderr, "no surfaceless support, cannot initialize\n");
-    //    exit(-1);
-    //}
+    kms_drm(context);
 
-    drmModeRes *resources;
-    drmModeConnector *connector;
-    drmModeEncoder *encoder;
-    drmModeModeInfo mode;
-    int i;
- 
-    /* Find the first available connector with modes */
- 
-    resources = drmModeGetResources(fd);
-    if (!resources) {
-        fprintf(stderr, "drmModeGetResources failed\n");
+    eglTerminate(context->egl_display);
+}
+
+void gbm_device(eglkms_context_t* context) {
+    context->gbm = gbm_create_device(context->fd);
+    if (context->gbm == NULL) {
+        fprintf(stderr, "couldn't create gbm device\n");
+        return;
+    }
+    egl_root(context);
+    gbm_device_destroy(context->gbm);
+}
+
+void device_filehandle(eglkms_context_t* context) {
+    context->fd = open("/dev/dri/card0", O_RDWR);
+    if (context->fd < 0) {
+        /* Probably permissions error */
+        fprintf(stderr, "couldn't open, skipping\n");
+        return;
+    }
+
+    gbm_device(context);
+
+    close(context->fd);
+}
+
+int32_t main(int argc, char **argv) {
+    eglkms_context_t* context;
+
+    context = malloc(sizeof(eglkms_context_t));
+    if (context == NULL) {
         return 1;
     }
 
-    for (i = 0; i < resources->count_connectors; i++) {
-        connector = drmModeGetConnector(fd, resources->connectors[i]);
-        if (connector == NULL)
-            continue;
-
-        if (connector->connection == DRM_MODE_CONNECTED &&
-            connector->count_modes > 0)
-            break;
-
-        drmModeFreeConnector(connector);
-    }
-
-    if (i == resources->count_connectors) {
-        fprintf(stderr, "No currently active connector found.\n");
-        return 1;
-    }
-
-    for (i = 0; i < resources->count_encoders; i++) {
-        encoder = drmModeGetEncoder(fd, resources->encoders[i]);
-
-        if (encoder == NULL)
-            continue;
-
-        if (encoder->encoder_id == connector->encoder_id)
-            break;
-
-        drmModeFreeEncoder(encoder);
-    }
-
-    mode = connector->modes[0];
-
-    fprintf(stderr, "W: %d, H: %d\n", mode.hdisplay, mode.vdisplay);
-
-    result = eglGetConfigs(display, NULL, 0, &num_configs);
-    if (result == EGL_FALSE) {
-        fprintf(stderr, "eglGetConfigs failed (getting the number of configs)\n");
-        return 1;
-    }
-
-    result = eglChooseConfig(display, attr_list, &config, 1, &num_configs);
-    if (result == EGL_FALSE) {
-        fprintf(stderr, "eglChooseConfig failed\n");
-        return 1;
-    }
-
-    eglBindAPI(EGL_OPENGL_API);
-    context = eglCreateContext(display, NULL, EGL_NO_CONTEXT, attr_context);
-    if (context == EGL_NO_CONTEXT) {
-        fprintf(stderr, "eglCreateContext failed\n");
-        return 1;
-    }
-
-    result = eglMakeCurrent(display, EGL_NO_SURFACE, EGL_NO_SURFACE, context);
-    if (result == EGL_FALSE) {
-        fprintf(stderr, "eglMakeCurrent failed\n");
-        return 1;
-    }
-
-
-
-/*
-    int minor_v, major_v;
-    uint32_t width, height;
-    int32_t success;
-
-    EGLBoolean result;
-    EGLDisplay display;
-
-    display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
-
-    if (display == EGL_NO_DISPLAY) {
-        fprintf(stderr, "eglGetDisplay failed\n");
-        return 1;
-    }
-
-    result = eglInitialize(display, &major_v, &minor_v);
-
-    if (result == EGL_FALSE) {
-        fprintf(stderr, "eglInitialize failed\n");
-        return 1;
-    }
-
-    fprintf(stderr, "EGL version %d.%d\n", major_v, minor_v);
-
-    result = eglGetConfigs(display, NULL, 0, &num_configs);
-
-    if (result == EGL_FALSE) {
-        fprintf(stderr, "eglGetConfigs failed (getting the number of configs)\n");
-        return 1;
-    }
-
-    result = eglChooseConfig(display, attr_list, &config, 1, &num_configs);
-
-    if (result == EGL_FALSE) {
-        fprintf(stderr, "eglChooseConfig failed\n");
-        return 1;
-    }
-
-    surface = eglCreateWindowSurface(display, config, window, NULL);
-
-    if (surface == EGL_NO_SURFACE) {
-        fprintf(stderr, "eglCreateWindowSurface failed\n");
-        return 1;
-    }
-
-    context = eglCreateContext(display, config, EGL_NO_CONTEXT, attr_context);
-
-    if (context == EGL_NO_CONTEXT) {
-        fprintf(stderr, "eglCreateContext failed\n");
-        return 1;
-    }
-
-    result = eglMakeCurrent(display, surface, surface, context);
-
-    if (result == EGL_FALSE) {
-        fprintf(stderr, "eglMakeCurrent failed\n");
-        return 1;
-    }
-
-    const GLubyte* extensions = glGetString(GL_EXTENSIONS);
-    fprintf(stderr, "extensions: %s\n", extensions);
-
-    const GLubyte* shaderv = glGetString(GL_SHADING_LANGUAGE_VERSION);
-    fprintf(stderr, "shaderv: %s\n", shaderv);
-
-    double physical_width = 0.7;
-
-    vrms_server_socket_init((int)width, (int)height, physical_width);
-
-    struct timespec ts;
-    ts.tv_sec = 0;
-    ts.tv_nsec = INNER_LOOP_INTERVAL_MS;
-
-    while (GL_TRUE) {
-        vrms_server_socket_display();
-        eglSwapBuffers(display, surface);
-        vrms_server_socket_process();
-        nanosleep(&ts, NULL);
-    }
-
-    vrms_server_socket_end();
-
-*/
+    device_filehandle(context);
 
     return 0;
 }
+
