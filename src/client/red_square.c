fb190be5d5a8ec6899d4d9f8614b4e398379d0c2
ceade <chris.eade@gmail.com>
2018-10-28 20:21:14 +0100

    Project do-not-declare-vars-at-the-top

--- src/client/red_square.c
+++ src/client/red_square.c
@@ -6,24 +6,20 @@
 #include "esm.h"
 
 int main(void) {
-    uint32_t scene_id;
-    uint32_t plane_id;
-    vrms_client_t* client;
-
-    client = vrms_connect();
+    vrms_client_t* client = vrms_connect();
     if (NULL == client) {
         fprintf(stderr, "Unable to connect\n");
         exit(1);
     }
 
-    scene_id = vrms_create_scene(client, "Test scene");
+    uint32_t scene_id = vrms_create_scene(client, "Test scene");
     if (scene_id == 0) {
         fprintf(stderr, "Unable to create scene\n");
         vrms_destroy_scene(client);
         exit(1);
     }
 
-    plane_id = vrms_geometry_plane(client, 2, 2, 1.0, 0.0, 0.0, 1.0);
+    uint32_t plane_id = vrms_geometry_plane(client, 2, 2, 1.0, 0.0, 0.0, 1.0);
     if (plane_id == 0) {
         fprintf(stderr, "Unable to create plane\n");
         vrms_destroy_scene(client);
