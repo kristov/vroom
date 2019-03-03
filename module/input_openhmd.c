657916f5ce97ba795dd16473e95326a88ebcd0a0
ceade <chris.eade@gmail.com>
2019-03-03 15:41:53 +0100

    Skip dummy OHMD devices

--- module/input_openhmd.c
+++ module/input_openhmd.c
@@ -37,6 +37,7 @@ int8_t hmd_init(vrms_module_t* module, hmd_t* hmd) {
         return 0;
     }
 
+    uint8_t found_hmd = 0;
     for (int i = 0; i < num_devices; i++) {
         int device_class = 0, device_flags = 0;
         const char* device_class_s[] = {"HMD", "Controller", "Generic Tracker", "Unknown"};
@@ -44,17 +45,26 @@ int8_t hmd_init(vrms_module_t* module, hmd_t* hmd) {
         ohmd_list_geti(ohmd_ctx, i, OHMD_DEVICE_CLASS, &device_class);
         ohmd_list_geti(ohmd_ctx, i, OHMD_DEVICE_FLAGS, &device_flags);
 
+        if (device_class == 0 && !(device_flags & OHMD_DEVICE_FLAGS_NULL_DEVICE)) {
+            found_hmd = 1;
+        }
+
         module->interface.debug(module, "device %d", i);
         module->interface.debug(module, "  vendor:  %s", ohmd_list_gets(ohmd_ctx, i, OHMD_VENDOR));
         module->interface.debug(module, "  product: %s", ohmd_list_gets(ohmd_ctx, i, OHMD_PRODUCT));
         module->interface.debug(module, "  path:    %s", ohmd_list_gets(ohmd_ctx, i, OHMD_PATH));
-        module->interface.debug(module, "  class:   %s", device_class_s[device_class > OHMD_DEVICE_CLASS_GENERIC_TRACKER ? 4 : device_class]);
+        module->interface.debug(module, "  class:   %s [%d]", device_class_s[device_class > OHMD_DEVICE_CLASS_GENERIC_TRACKER ? 4 : device_class], device_class);
         module->interface.debug(module, "  flags:   %02x",  device_flags);
         module->interface.debug(module, "    null device:         %s", device_flags & OHMD_DEVICE_FLAGS_NULL_DEVICE ? "yes" : "no");
         module->interface.debug(module, "    rotational tracking: %s", device_flags & OHMD_DEVICE_FLAGS_ROTATIONAL_TRACKING ? "yes" : "no");
         module->interface.debug(module, "    positional tracking: %s", device_flags & OHMD_DEVICE_FLAGS_POSITIONAL_TRACKING ? "yes" : "no");
         module->interface.debug(module, "    left controller:     %s", device_flags & OHMD_DEVICE_FLAGS_LEFT_CONTROLLER ? "yes" : "no");
         module->interface.debug(module, "    right controller:    %s", device_flags & OHMD_DEVICE_FLAGS_RIGHT_CONTROLLER ? "yes" : "no");
+    }
+
+    if (!found_hmd) {
+        module->interface.debug(module, "did not find a non-dummy HMD device");
+        return 0;
     }
 
     hmd->ohmd_active_hmd = ohmd_list_open_device(ohmd_ctx, 0);
